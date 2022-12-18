abcc4ade80f05fa572f47995208b9654ef0a604e
Ilippy <Ilippyk@gmail.com>
2022-12-19 01:41:04 +0300

    add Sum Array project

---
+++ SumArray/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[] array = { 3, 7, 5, 4, 9};
+        int result = SumArray(array);
+        System.Console.Write("Сумма массива чисел ");
+        Console.Write("[{0}]", string.Join(", ", array) );
+        System.Console.Write($" равна {result}");
+    }
+
+    static int SumArray(int[] array, int i = 0){
+        if (i >= array.Length) return 0;
+        int result = SumArray(array, i+1);
+        return array[i] + result;
+    }
+}
